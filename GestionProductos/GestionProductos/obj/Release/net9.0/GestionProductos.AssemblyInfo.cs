e5585e96907ffda10fbce348d6ead823fee46c13
OppaDev <jlobando2@espe.edu.ec>
2025-01-14 07:11:24 -0500

    route of products appi OppaDev-V0002

--- GestionProductos/GestionProductos/obj/Release/net9.0/GestionProductos.AssemblyInfo.cs
+++ GestionProductos/GestionProductos/obj/Release/net9.0/GestionProductos.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionProductos")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+783ba00dfd35789af6eee8e368e0453f62a6ac65")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionProductos")]
